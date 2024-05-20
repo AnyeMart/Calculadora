95c4cfb9f5df77ea7bafb228b2ee9cf81dd96ba9
anyelo Martinez <anyelo.martinez@gmail.com>
2024-05-19 20:22:45 -0500

    Commit 2

---
+++ Multiplicacion.js
@@ -0,0 +1,6 @@
+function Mult(numero1, numero2)
+{
+    let resultado;
+    resultado = numero1 * numero2;
+    return resultado;
+}
---
+++ Suma.js
@@ -0,0 +1,6 @@
+function sumar(numero1, numero2)
+{
+    let resultado;
+    resultado = numero1 + numero2;
+    return resultado;
+}
